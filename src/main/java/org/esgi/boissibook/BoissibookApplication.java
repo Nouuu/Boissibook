68639d52a7de3191d0b931a7b2cbcb3e742fb0f6
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-04-24 16:56:16 +0200

    init project

---
+++ src/main/java/org/esgi/boissibook/BoissibookApplication.java
@@ -0,0 +1,13 @@
+package org.esgi.boissibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoissibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoissibookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esgi/boissibook/BoissibookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esgi.boissibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoissibookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
