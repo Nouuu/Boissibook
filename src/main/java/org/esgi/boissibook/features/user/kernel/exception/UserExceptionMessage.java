b1e2f677abdf3a99274e0ee1bd5266c61fd21814
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-08 15:46:11 +0200

    change exception message property name

--- src/main/java/org/esgi/boissibook/features/user/kernel/exception/UserExceptionMessage.java
+++ src/main/java/org/esgi/boissibook/features/user/kernel/exception/UserExceptionMessage.java
@@ -4,14 +4,14 @@ public enum UserExceptionMessage {
     USER_NOT_FOUND("User not found"),
     ;
 
-    private final String s;
+    private final String message;
 
-    UserExceptionMessage(String s) {
-        this.s = s;
+    UserExceptionMessage(String message) {
+        this.message = message;
     }
 
     @Override
     public String toString() {
-        return s;
+        return message;
     }
 }
