78af55a8629a83c78ae6fd71309ba890f1983f51
Swann HERRERA <swann@devloup.dev>
2022-06-05 17:37:26 +0200

    ajout de check sur les invariants du domain

--- src/main/java/org/esgi/boissibook/features/readlist/domain/BookReviewId.java
+++ src/main/java/org/esgi/boissibook/features/readlist/domain/BookReviewId.java
@@ -1,7 +1,10 @@
 package org.esgi.boissibook.features.readlist.domain;
 
+import java.util.Objects;
+
 public record BookReviewId(String reviewId) {
     public static BookReviewId of(String reviewId) {
+        Objects.requireNonNull(reviewId, "reviewId cannot be null");
         return new BookReviewId(reviewId);
     }
 }
