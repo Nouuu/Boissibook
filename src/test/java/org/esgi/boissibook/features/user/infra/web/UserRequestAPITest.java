a7f3da26cd9c383f3c388a7878d7a8274475603d
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-11 17:13:22 +0200

    Create user api request ete tests

---
+++ src/test/java/org/esgi/boissibook/features/user/infra/web/UserRequestAPITest.java
@@ -0,0 +1,90 @@
+package org.esgi.boissibook.features.user.infra.web;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.esgi.boissibook.PostgresIntegrationTest;
+import org.esgi.boissibook.features.user.domain.User;
+import org.esgi.boissibook.features.user.infra.UserMapper;
+import org.esgi.boissibook.features.user.infra.repository.JPAUserRepository;
+import org.esgi.boissibook.features.user.infra.web.response.UserResponse;
+import org.esgi.boissibook.kernel.repository.UserId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
+
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
+@Testcontainers
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+class UserRequestAPITest extends PostgresIntegrationTest {
+
+    final User validUser1 = new User(
+        UserId.nextId(),
+        "user1",
+        "user1@mail.com",
+        "password123");
+
+    final User validUser2 = new User(
+        UserId.nextId(),
+        "user2",
+        "user2@mail.com",
+        "password123");
+
+    final User invalidUser1 = new User(
+        UserId.nextId(),
+        "user1",
+        "user",
+        "password123");
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    JPAUserRepository jpaUserRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+    @Test
+    void getAllUsers() {
+        jpaUserRepository.save(UserMapper.toUserEntity(validUser1));
+        jpaUserRepository.save(UserMapper.toUserEntity(validUser2));
+
+
+        var userResources = when()
+            .get("/users")
+            .then()
+            .statusCode(200)
+            .extract()
+            .body().jsonPath().getList(".", UserResponse.class);
+
+        assertThat(userResources).hasSize(2)
+            .containsExactly(
+                UserMapper.toUserResponse(validUser1),
+                UserMapper.toUserResponse(validUser2)
+            );
+    }
+
+/*    @Test
+    void getBookByIdNotFound() {
+        when()
+            .get("/books/{id}", book1.id().value())
+            .then()
+            .statusCode(404);
+    }*/
+}
+
