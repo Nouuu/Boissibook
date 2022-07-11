921c43c4bce1c9dc18290d74f23a49be9e1d195e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-07-11 17:48:30 +0200

    fix user create controller link to

--- src/main/java/org/esgi/boissibook/features/user/infra/web/UserCommandController.java
+++ src/main/java/org/esgi/boissibook/features/user/infra/web/UserCommandController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.esgi.boissibook.features.user.domain.UserCommandHandler;
-import org.esgi.boissibook.features.user.domain.UserQueryHandler;
 import org.esgi.boissibook.features.user.infra.UserMapper;
 import org.esgi.boissibook.features.user.infra.web.request.CreateUserRequest;
 import org.esgi.boissibook.features.user.infra.web.request.UpdateUserRequest;
@@ -58,7 +57,7 @@ public class UserCommandController {
     public ResponseEntity<Void> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
         var createUser = UserMapper.toUser(createUserRequest);
         var userId = userCommandHandler.createUser(createUser);
-        return ResponseEntity.created(linkTo(methodOn(UserQueryHandler.class).getUser(userId)).toUri()).build();
+        return ResponseEntity.created(linkTo(methodOn(UserRequestController.class).getUserById(userId.value())).toUri()).build();
     }
 
     @Operation(summary = "Update existing user")
