8f95c5c6a1da5d1745d98064b94eb6b1af09018a
pooetitu <pooetitu@gmail.com>
2022-07-12 13:22:26 +0200

    Fix achievement entities

--- src/main/java/org/esgi/boissibook/features/achievement/infra/repository/UserStatisticsEntity.java
+++ src/main/java/org/esgi/boissibook/features/achievement/infra/repository/UserStatisticsEntity.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user-statistics")
+@Table(name = "user_statistics")
 public class UserStatisticsEntity {
     @Id
     private String id;
